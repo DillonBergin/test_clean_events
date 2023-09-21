8eb004a0954be9428d3ab06b942f6a16919d15f7
Dillon Bergin <news.dillonbergin@gmail.com>
2023-09-21 13:25:50 -0400

    Re name test scripts and data

--- test_script.py
+++ test_script.py
@@ -9,18 +9,19 @@ state_fips = fips["STATEFP"]
 fips_list = state_fips.to_string()
 fips_list = "","".join(map(str, fips_list))
 
+lines = ""
 current_line = ""
-with open("test_lines.txt") as input_file:
-    for line in input_file:
+with open("example_lines.txt") as file:
+    for line in file:
         previous_line = current_line
         current_line = line
         if not line.startswith("TT" or fips_list):
-            current_line = previous_line + current_line
-    with open('test.csv', 'w') as output_file:
-        writer = csv.writer(output_file)
-        writer.writerows(line)
-
-                
+            new_line = previous_line + current_line
+        else:
+            new_line = current_line
+    with open('test_output.csv', 'w') as output_file:
+            writer = csv.writer(output_file, delimiter = "|")
+            writer.writerows(new_line)
 
 
     
